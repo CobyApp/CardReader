f0ccd525f9e07e4ecc602217961dae991b21574f
coby <coby5502@gmail.com>
2024-06-10 10:39:01 +0900

    Initial Commit

---
+++ CardReader/CardReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardReaderApp.swift
+//  CardReader
+//
+//  Created by Coby Kim on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CardReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
